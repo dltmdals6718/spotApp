7bac6fa51ad6447001afe489d4acb2737e1a7402
LSM <dltmdals6718@naver.com>
2023-11-02 17:02:00 +0900

    시작 위치 바꾸기 및 마커 찍어보기

--- app/src/main/java/com/example/spotapp/MainActivity.java
+++ app/src/main/java/com/example/spotapp/MainActivity.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 
 import com.kakao.vectormap.KakaoMap;
 import com.kakao.vectormap.KakaoMapReadyCallback;
+import com.kakao.vectormap.LatLng;
 import com.kakao.vectormap.MapLifeCycleCallback;
 import com.kakao.vectormap.MapView;
+import com.kakao.vectormap.label.Label;
+import com.kakao.vectormap.label.LabelLayer;
+import com.kakao.vectormap.label.LabelOptions;
+import com.kakao.vectormap.label.LabelStyle;
+import com.kakao.vectormap.label.LabelStyles;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +41,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMapReady(KakaoMap kakaoMap) {
                 // 인증 후 API 가 정상적으로 실행될 때 호출됨
+                LabelStyles styles = kakaoMap.getLabelManager()
+                        .addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.blue_marker)));
+
+                LabelOptions options = LabelOptions.from(LatLng.from(35.24589,128.6920))
+                        .setStyles(styles);
+
+                LabelLayer layer = kakaoMap.getLabelManager().getLayer();
+
+                Label label = layer.addLabel(options);
+            }
+
+            @Override
+            public LatLng getPosition() {
+                // 지도 시작 시 위치 좌표를 설정
+                return LatLng.from(35.24589, 128.6920);
             }
         });
     }
