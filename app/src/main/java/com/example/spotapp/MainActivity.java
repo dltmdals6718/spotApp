ebc7fe48df83e378d8be9f70fc6796cc53e50949
LSM <dltmdals6718@naver.com>
2023-11-04 20:59:30 +0900

    fragment의 요소에 findViewById 사용시 null 해결 fragment의 onCreateView 시점에 findViewById를 사용하자.

--- app/src/main/java/com/example/spotapp/MainActivity.java
+++ app/src/main/java/com/example/spotapp/MainActivity.java
@@ -10,16 +10,6 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.kakao.vectormap.KakaoMap;
-import com.kakao.vectormap.KakaoMapReadyCallback;
-import com.kakao.vectormap.LatLng;
-import com.kakao.vectormap.MapLifeCycleCallback;
-import com.kakao.vectormap.MapView;
-import com.kakao.vectormap.label.Label;
-import com.kakao.vectormap.label.LabelLayer;
-import com.kakao.vectormap.label.LabelOptions;
-import com.kakao.vectormap.label.LabelStyle;
-import com.kakao.vectormap.label.LabelStyles;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -71,38 +61,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        MapView mapView = findViewById(R.id.map_view);
-        System.out.println("mapView = " + mapView);
-//        mapView.start(new MapLifeCycleCallback() {
-//            @Override
-//            public void onMapDestroy() {
-//                // 지도 API 가 정상적으로 종료될 때 호출됨
-//            }
-//
-//            @Override
-//            public void onMapError(Exception error) {
-//                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
-//            }
-//        }, new KakaoMapReadyCallback() {
-//            @Override
-//            public void onMapReady(KakaoMap kakaoMap) {
-//                // 인증 후 API 가 정상적으로 실행될 때 호출됨
-//                LabelStyles styles = kakaoMap.getLabelManager()
-//                        .addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.blue_marker)));
-//
-//                LabelOptions options = LabelOptions.from(LatLng.from(35.24589,128.6920))
-//                        .setStyles(styles);
-//
-//                LabelLayer layer = kakaoMap.getLabelManager().getLayer();
-//
-//                Label label = layer.addLabel(options);
-//            }
-//
-//            @Override
-//            public LatLng getPosition() {
-//                // 지도 시작 시 위치 좌표를 설정
-//                return LatLng.from(35.24589, 128.6920);
-//            }
-//        });
     }
 }
--- app/src/main/java/com/example/spotapp/SpotFragment.java
+++ app/src/main/java/com/example/spotapp/SpotFragment.java
@@ -8,11 +8,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kakao.vectormap.KakaoMap;
+import com.kakao.vectormap.KakaoMapReadyCallback;
+import com.kakao.vectormap.LatLng;
+import com.kakao.vectormap.MapLifeCycleCallback;
+import com.kakao.vectormap.MapView;
+import com.kakao.vectormap.label.Label;
+import com.kakao.vectormap.label.LabelLayer;
+import com.kakao.vectormap.label.LabelOptions;
+import com.kakao.vectormap.label.LabelStyle;
+import com.kakao.vectormap.label.LabelStyles;
+
 public class SpotFragment extends Fragment {
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_spot, container, false);
+        MapView mapView = view.findViewById(R.id.map_view);
+        mapView.start(new MapLifeCycleCallback() {
+            @Override
+            public void onMapDestroy() {
+                // 지도 API 가 정상적으로 종료될 때 호출됨
+            }
+
+            @Override
+            public void onMapError(Exception error) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
+            }
+        }, new KakaoMapReadyCallback() {
+            @Override
+            public void onMapReady(KakaoMap kakaoMap) {
+
+                // 인증 후 API 가 정상적으로 실행될 때 호출됨
+                LabelStyles styles = kakaoMap.getLabelManager()
+                        .addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.blue_marker)));
+
+                LabelOptions options = LabelOptions.from(LatLng.from(35.24589,128.6920))
+                        .setStyles(styles);
+
+                LabelLayer layer = kakaoMap.getLabelManager().getLayer();
+
+                Label label = layer.addLabel(options);
+            }
+
+            @Override
+            public LatLng getPosition() {
+                // 지도 시작 시 위치 좌표를 설정
+                return LatLng.from(35.24589, 128.6920);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spot, container, false);
+        return view;
     }
 }
