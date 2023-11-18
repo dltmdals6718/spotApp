60a0c34a0e31c8722bb1ea207823944250dca760
LSM <dltmdals6718@naver.com>
2023-11-18 17:31:58 +0900

    local.properties를 읽기 위한 과정 BuildConfig.이름으로 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,14 @@
+import java.io.FileInputStream
+import java.util.Properties;
+
 plugins {
     id("com.android.application")
 }
+
+
+// local.properties 읽기 과정
+var properties = Properties()
+properties.load(FileInputStream("local.properties"))
 
 android {
     namespace = "com.example.spotapp"
@@ -14,6 +22,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // 읽기 과정 - BuildConfig에 등록
+        buildConfigField("String", "SERVER_IP", properties.getProperty("serverAddress"))
     }
 
     buildTypes {
@@ -29,6 +40,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    // 읽기 과정
+    buildFeatures {
+        buildConfig = true
+    }
+
 }
 
 dependencies {
@@ -37,6 +54,10 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.kakao.maps.open:android:2.6.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
